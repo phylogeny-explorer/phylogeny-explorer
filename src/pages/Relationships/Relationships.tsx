e26ab61316a7682ea596eacec41d936b367fcaf6
clickonrefresh <clickonrefresh@gmail.com>
2021-10-04 17:22:53 +0000

    Start of nextjs migration

--- src/pages/Relationships/Relationships.tsx
+++ src/pages/Relationships/Relationships.tsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import Page from 'components/Page';
-import NavBar from 'components/NavBar';
-import Search from 'components/Search';
-import Button from 'components/Button';
-import { HeadingLarge } from 'components/Typography';
+import Page from "components/Page";
+import NavBar from "components/NavBar";
+import Search from "components/Search";
+import Button from "components/Button";
+import { HeadingLarge } from "components/Typography";
+import styled from "styled-components";
 
-import { Wrapper } from './Relationships.styled';
-import MRCA from './graphql/mrca';
+// import { Wrapper } from "./Relationships.styled";
+import MRCA from "../../lib/graphql/mrca";
 
 const Relationships = () => {
   const { nodeId } = useParams<{ nodeId: string }>();
 
   const [clade1, setClade1] = useState(nodeId);
-  const [clade2, setClade2] = useState('');
+  const [clade2, setClade2] = useState("");
   const [skip, setSkip] = useState(true);
 
   const { loading, data } = useQuery(MRCA, {
@@ -23,13 +24,30 @@ const Relationships = () => {
     variables: { clade1, clade2 },
   });
 
+
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-gap: ${(props) => props.theme.xlargeSpacer}px;
+  padding: ${(props) => props.theme.xlargeSpacer}px;
+  max-width: 400px;
+`;
+
   return (
     <Page>
       <NavBar />
       <Wrapper>
         <HeadingLarge>Most Recent Common Ancestor</HeadingLarge>
-        <Search value={clade1} handleSelect={id => setClade1(id)} isContained />
-        <Search value={clade2} handleSelect={id => setClade2(id)} isContained />
+        <Search
+          value={clade1}
+          handleSelect={(id) => setClade1(id)}
+          isContained
+        />
+        <Search
+          value={clade2}
+          handleSelect={(id) => setClade2(id)}
+          isContained
+        />
         <Button
           text="search for common ancestor"
           loading={loading}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,57 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx:DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#06C9AF" />
+          <meta
+            name="description"
+            content="Web client for the Phylogeny Explorer Project"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#06c9af" />
+          <meta
+            name="apple-mobile-web-app-title"
+            content="Phylogeny Explorer"
+          />
+          <meta name="application-name" content="Phylogeny Explorer" />
+          <meta name="msapplication-TileColor" content="#06c9af" />
+          <meta name="theme-color" content="#2b3034" />
+          <title>Phylogeny Explorer</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
