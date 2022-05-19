ebc7361ee954a16400274bab92d5e680caffeef8
Patrick Mitchell <pat.mitchell@gmail.com>
2022-05-18 23:53:51 -0400

    Pass token to graphql in Auth header

--- src/lib/hooks/useUser.ts
+++ src/lib/hooks/useUser.ts
@@ -3,27 +3,27 @@ import { useRouter } from 'next/router';
 import { LoginContext } from 'context/LoginContext';
 
 const useUser = ({ redirectTo = '', redirectIfFound = false } = {}) => {
-  const router = useRouter();
-  const { isLoggedIn, isLoadingUser, setSession, removeSession } = useContext(
-    LoginContext
-  );
+	const router = useRouter();
+	const { isLoggedIn, isLoadingUser, token, setSession, removeSession } = useContext(
+		LoginContext
+	);
 
-  useEffect(() => {
-    // if no redirect needed, just return
-    // if user data not yet there then don't do anything yet
-    if (!redirectTo || isLoadingUser) return;
+	useEffect(() => {
+		// if no redirect needed, just return
+		// if user data not yet there then don't do anything yet
+		if (!redirectTo || isLoadingUser) return;
 
-    if (
-      // If redirectTo is set, redirect if the user was not found.
-      (!redirectIfFound && !isLoggedIn) ||
-      // If redirectIfFound is also set, redirect if the user was found
-      (redirectIfFound && isLoggedIn)
-    ) {
-      router.push(redirectTo);
-    }
-  }, [isLoggedIn, isLoadingUser, redirectIfFound, redirectTo, router]);
+		if (
+			// If redirectTo is set, redirect if the user was not found.
+			(!redirectIfFound && !isLoggedIn) ||
+			// If redirectIfFound is also set, redirect if the user was found
+			(redirectIfFound && isLoggedIn)
+		) {
+			router.push(redirectTo);
+		}
+	}, [isLoggedIn, isLoadingUser, redirectIfFound, redirectTo, router]);
 
-  return { isLoggedIn, isLoadingUser, setSession, removeSession };
+	return { isLoggedIn, isLoadingUser, token, setSession, removeSession };
 };
 
 export default useUser;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,67 +2,79 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import reset from 'styled-reset';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import theme from 'theme';
 import LoginProvider from 'context/LoginContext';
 import Toast from 'components/Toast';
+import { AUTH_USER_TOKEN_KEY } from 'consts';
 
 const env = process.env.NEXT_PUBLIC_API_BASE || 'dev';
 
 export const APOLLO_LINK = {
-  local: 'http://localhost:4000/',
-  dev: 'https://api.phylogenyexplorerproject.co.uk/',
-  prod: 'https://api.phylogenyexplorerproject.co.uk/',
+	local: 'http://localhost:4000/',
+	dev: 'https://api.phylogenyexplorerproject.co.uk/',
+	prod: 'https://api.phylogenyexplorerproject.co.uk/',
 }[env];
 
 const client = new ApolloClient({
-  uri: APOLLO_LINK,
-  cache: new InMemoryCache(),
+	cache: new InMemoryCache(),
+	link: setContext((_, { headers }) => {
+		//TODO: would be better to read the token from useUser();
+		const token = localStorage.getItem(AUTH_USER_TOKEN_KEY);
+		return {
+			headers: {
+				...headers,
+				authorization: token ? `Bearer ${token}` : '',
+			}
+		};
+	})
+		.concat(new HttpLink({ uri: APOLLO_LINK }))
 });
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
-  html {
-    @media screen and (max-width: 320px) {
-      font-size: 14px;
-    }
-  }
-  body {
-    margin: 0;
-    font-family: 'Roboto', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  a {
-    color: ${theme.primary};
-    text-decoration-color: transparent;
-    transition: text-decoration 500ms;
-    &:hover {
-      text-decoration-color: ${theme.primary};
-    }
-  }
+	${reset}
+	html {
+		@media screen and (max-width: 320px) {
+			font-size: 14px;
+		}
+	}
+	body {
+		margin: 0;
+		font-family: 'Roboto', sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+	a {
+		color: ${theme.primary};
+		text-decoration-color: transparent;
+		transition: text-decoration 500ms;
+		&:hover {
+			text-decoration-color: ${theme.primary};
+		}
+	}
 `;
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return (
-    <LoginProvider>
-      <ApolloProvider client={client}>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          <Toast />
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-          </Head>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </ApolloProvider>
-    </LoginProvider>
-  );
+	return (
+		<LoginProvider>
+			<ApolloProvider client={client}>
+				<GlobalStyle />
+				<ThemeProvider theme={theme}>
+					<Toast />
+					<Head>
+						<meta
+							name="viewport"
+							content="width=device-width, initial-scale=1"
+						/>
+					</Head>
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</ApolloProvider>
+		</LoginProvider>
+	);
 };
 
 export default App;
